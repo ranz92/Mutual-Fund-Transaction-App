4f8d2b9cd0efe5a4b5b0476359923f4c44b92b71
Jiayi Zhu <quriola@gmail.com>
2015-01-30 20:45:45 -0500

    updated sellfund

--- task7-financial-runnable-new/src/controller/ConfirmSellAction.java
+++ task7-financial-runnable-new/src/controller/ConfirmSellAction.java
@@ -81,7 +81,7 @@ public class ConfirmSellAction extends Action {
 				transaction.setShares(l);
 				
 			} catch(NumberFormatException e) {
-	//			errors.add("Please enter numbers");
+				errors.add("Please enter numbers");
 				e.printStackTrace();
 			}
 			
